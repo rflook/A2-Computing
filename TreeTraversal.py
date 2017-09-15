8de863badc465c12720ecb252e2c6f2bc2ac998e
MrFlook <robert.flook@gmail.com>
2017-09-15 15:26:35 +0100

    Update TreeTraversal.py

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -37,7 +37,6 @@ def inOrder(tree):
     steps = 0
     currentPos = 0
     complete = False
-    prevDir = None
 
     #stack.append(tree[currentPos])
 
@@ -46,24 +45,29 @@ def inOrder(tree):
         # if we can do neither then print the value and go up one level
         if (tree[currentPos][2] is None) and (tree[currentPos][1] is None):
             outputs.append(tree[currentPos][0])
-            prevDir = "U"
 
             # unwind the by going back to the last right branch
             counter = len(stack)-1
-            while stack[counter][2] is None:
+
+            if counter > -1:
+                while stack[counter][2] is None:
+
+                    outputs.append(stack[counter][0])
+                    currentPos = stack[counter][2]
+                    stack.pop()
+                    if counter >= 1:
+                        counter -= 1
+                    else:
+                        break
+
+            if len(stack) > 0:
                 outputs.append(stack[counter][0])
                 currentPos = stack[counter][2]
                 stack.pop()
-                counter -= 1
-
-            outputs.append(stack[counter][0])
-            currentPos = stack[counter][2]
-            stack.pop()
 
         # check if we can only go right
         elif (tree[currentPos][2] is not None) and (tree[currentPos][1] is None):
             # if can only go right, we need to add to stack change right ptr to none and output the value
-            prevDir = "D"
             newPos = tree[currentPos][2]
             outputs.append(tree[currentPos][0])
             # stack.append(tree[currentPos])
@@ -73,31 +77,12 @@ def inOrder(tree):
 
         #check if we can go left
         elif (tree[currentPos][1] is not None):
-            prevDir = "D"
             newPos = tree[currentPos][1]
             stack.append(tree[currentPos])
             tree[currentPos][1] = None
             currentPos = newPos
             steps += 1
 
-
-
-
-
-
-
         if len(outputs) == len(tree):
             print(outputs)
             complete = True
-
-
-
-
-
-#run the code
-#vals = [7, 4, 10, 3, 5, 6, 12, 15]
-vals = [7,25,42,99,65,82,61,54,17,29,13,8,12]
-#vals = [10,6,7,1,4,3,2,89,45,20,18,21]
-tree = createTree(vals)
-#print(tree)
-inOrder(tree)
